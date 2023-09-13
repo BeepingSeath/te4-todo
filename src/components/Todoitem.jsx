c75bfdee730ba1143624daadeeb4f8495b845cf5
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2023-09-13 14:54:24 +0200

    Working Checklist with React

---
+++ src/components/Todoitem.jsx
@@ -0,0 +1,18 @@
+import './Todoitem.css'
+
+function Todoitem (props) {
+    let {id, completed, label } = props;
+
+    return (
+        <li className="todo-item">
+            <label>{ label }</label>
+            <input 
+                checked = { completed }
+                onChange = { () => { props.toggleTaskCompleted(id) } }
+                type = "checkbox"/>
+            <button onClick={() => { props.deleteTodo(id) }}/>
+        </li>
+    )
+}
+
+export default Todoitem
---
+++ src/components/Todolist.jsx
@@ -0,0 +1,92 @@
+import Todoitem from './Todoitem'
+import './Todolist.css'
+import {useEffect, useState} from 'react'
+
+function Todolist() {
+  const [todos, setTodos] = useState(() => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+  })
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const deleteAll = () => {
+    setTodos([]); 
+    localStorage.setItem('index', 0) // create number that rises every added item to use for items id / resets it to zero 
+}
+
+const addTodo = () => {
+    const newTodo = document.getElementById('newTodo').value;
+    if (newTodo === '') return
+    const newTodos = [...todos, { id: localStorage.getItem('index'), label: newTodo, completed: false }]    //uses locally stored number to get index
+    setTodos(newTodos)
+    localStorage.setItem('index', parseInt(localStorage.getItem('index'))+1 || parseInt(todos.length)+1)    // adds number +1 for ever post created
+}
+
+  const toggleTaskCompleted = (id) => {
+    const newTodos = todos.map(todo => {
+      const newTodo = todo;
+      if (todo.id === id) {
+        newTodo.completed = !newTodo.completed;
+      }
+      return newTodo;
+    })
+    setTodos(newTodos);
+  }
+
+  const checkAll = (id) => {
+    let numTrue = 0;
+    let numFalse = 0;
+    todos.forEach(todo => {
+        console.log(todo.completed);
+        if(todo.completed == true){
+            numTrue++;
+        } else { 
+            numFalse++;
+        }
+        console.log(numTrue);
+        console.log(numFalse);
+    });
+    const newTodos = todos.map(todo => {
+      const newTodo = todo;
+      console.log(todos.length);
+      if(numTrue == todos.length) {
+        newTodo.completed = false;
+        return newTodo;
+      } else if(numTrue < numFalse) {
+        newTodo.completed = true;
+        return newTodo;
+      } else {
+        newTodo.completed = true;
+        return newTodo;
+      }
+    })
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (id) => {
+    const length = todos.length;
+    const filtered = todos.filter(todo => todo.id !== id)
+    setTodos(filtered);
+  }
+
+  return ( <>
+    <input id="newTodo" type="text" placeholder="Skriv in en sak!"/>
+    <button onClick={() => { addTodo() }}>Lägg till</button>
+    <button onClick={() => { checkAll() }}>Checka alla</button>
+    <button onClick={() => { deleteAll() }}>Ta bort allt</button>
+    <ul className="todo-list">
+        {todos.map((todo, index) => <Todoitem
+            key={ index }
+            id={todo.id}
+            label={todo.label}
+            completed={todo.completed}
+            toggleTaskCompleted={toggleTaskCompleted}
+            deleteTodo={deleteTodo}/>)}
+    </ul> 
+  </>
+  )
+}
+
+export default Todolist
